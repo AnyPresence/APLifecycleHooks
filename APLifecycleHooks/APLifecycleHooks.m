d5983d26968cf980fbb2cd523458788c900ad7ea
David Benko <DavidBenko@users.noreply.github.com>
2015-02-11 15:31:26 -0500

    Update to work with APSDK R7

--- APLifecycleHooks/APLifecycleHooks.m
+++ APLifecycleHooks/APLifecycleHooks.m
@@ -8,7 +8,12 @@
 
 #import "APLifecycleHooks.h"
 #import "Swizzlean.h"
+#import <objc/objc-runtime.h>
 #import <APSDK/APRequest.h>
+
+@interface NSString (Inflections)
+- (NSString *)titleize;
+@end
 
 @interface APRequest (APLifecycleHooks)
 @property (nonatomic, strong) NSString * verb;
@@ -19,105 +24,162 @@
 @property (nonatomic, strong) APRequestCallback callback;
 @property (nonatomic, strong) NSError * error;
 @property (nonatomic, strong) NSMutableData * downloadedData;
--(void)reset;
-- (NSURLRequest *)request:(NSString *)verb to:(NSURL *)URL body:(NSData *)body contentType:(NSString *)type;
+@property (nonatomic, strong) NSURLResponse *response;
+
+
+- (void)reset;
+- (NSURLRequest *)request;
 - (void)checkResponse:(NSURLResponse *)response error:(NSError **)error;
+@end
+
+#pragma clang diagnostic ignored "-Wincomplete-implementation"
+@implementation APRequest (APLifecycleHooks)
+@dynamic connection,
+contentType,
+data,
+error,
+callback,
+URL,
+verb,
+downloadedData;
+
+- (NSURLResponse *)response {
+    return objc_getAssociatedObject(self, @selector(response));
+}
+
+- (void)setResponse:(NSURLResponse *)response {
+    objc_setAssociatedObject(self, @selector(response), response, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+
 @end
 
 @implementation APLifecycleHooks
 
-static bool sdkShouldLog;
-
-+ (void)initialize
-{
-    static BOOL initialized = NO;
-    if(!initialized)
-    {
-        initialized = YES;
-        sdkShouldLog = true;
++ (void)initialize {
+    if (self == [APLifecycleHooks class]) {
+        [self connectionReceievedResponse:nil];
     }
 }
 
-+(void)setSDKLogging:(BOOL)shouldLog{
-    sdkShouldLog = shouldLog;
-}
-
-+(void)synchronousConnectionFinished:(void (^)(NSData *downloadedData, NSError *error))hook{
-    Swizzlean *swizzle = [[Swizzlean alloc] initWithClassToSwizzle:[APRequest class]];
-    swizzle.resetWhenDeallocated = NO;
-    [swizzle swizzleInstanceMethod:@selector(load:) withReplacementImplementation:^NSData* (APRequest *_self, NSError ** error){
-        NSURLRequest * request = [_self request:_self.verb to:_self.URL body:_self.data contentType:_self.contentType];
-        
-        if (sdkShouldLog){
-            NSLog(@"\n%@ %@\n%@", _self.verb, [_self.URL absoluteString], [[NSString alloc] initWithData:_self.data encoding:NSUTF8StringEncoding]);
-        }
-        
-        NSHTTPURLResponse * response;
-        NSData * data = [NSURLConnection sendSynchronousRequest:request
-                                              returningResponse:&response
-                                                          error:error];
-        
-        if (sdkShouldLog) {
-            NSLog(@"%@", [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]);
-        }
-        
-        [_self checkResponse:response error:error];
-        if (error && *error) {
-            [[NSNotificationCenter defaultCenter] postNotificationName:kAPRequestErrorNotification
-                                                                object:@{ kAPRequestErrorNotificationErrorKey: *error }];
-        }
-        if (hook) {
-            hook(data, *error);
-        }
-        return data;
-    }];
-}
-
-+(void)asyncConnectionStarted:(void (^)(NSURLRequest * req, NSURLConnection *conn))hook{
++ (void)connectionWillStart:(NSURLRequest * (^)(NSURLRequest * req))connectionWillStartCallback didStart:(void (^)(NSURLRequest * req, NSURLConnection *conn))connectionDidStartCallback{
     Swizzlean *swizzle = [[Swizzlean alloc] initWithClassToSwizzle:[APRequest class]];
     swizzle.resetWhenDeallocated = NO;
     [swizzle swizzleInstanceMethod:@selector(loadAsync:) withReplacementImplementation:^(APRequest *_self, APRequestCallback callback){
+        
         if (_self.connection) {
             @throw [NSException exceptionWithName:kAPRequestConcurrentRequestException
                                            reason:@"Concurrent asynchronous requests not supported"
                                          userInfo:nil];
         }
         
-        NSURLRequest * request = [_self request:_self.verb to:_self.URL body:_self.data contentType:_self.contentType];
+        NSURLRequest * request = [_self request];
+        
+        
+        // Hijack Request Here
+        if (connectionWillStartCallback) {
+            request = connectionWillStartCallback(request);
+        }
         
         [_self reset];
+        
         _self.connection = [[NSURLConnection alloc] initWithRequest:request
                                                           delegate:_self
                                                   startImmediately:NO];
+        
         _self.downloadedData = [NSMutableData new];
         _self.callback = callback;
         
-        if (sdkShouldLog) {
-            NSLog(@"\n%@ %@\n%@", _self.verb, [_self.URL absoluteString], [[NSString alloc] initWithData:_self.data encoding:NSUTF8StringEncoding]);
-        }
+        
+#ifdef __VERBOSE
+        NSLog(@"\n%@ %@\n%@", _self.verb, [_self.URL absoluteString], [[NSString alloc] initWithData:_self.data encoding:NSUTF8StringEncoding]);
+#endif
         
         [_self.connection start];
         
-        if (hook){
-            hook(request,_self.connection);
+        if (connectionDidStartCallback) {
+            connectionDidStartCallback(request,_self.connection);
         }
         
     }];
 }
 
-+(void)asyncConnectionFinished:(BOOL (^)(NSMutableData *downloadedData, NSError *error))hook{
++ (void)connectionReceievedResponse:(void (^)(NSURLConnection *connection, NSURLResponse *response))connectionReceievedResponseCallback{
+    Swizzlean *swizzle = [[Swizzlean alloc] initWithClassToSwizzle:[APRequest class]];
+    swizzle.resetWhenDeallocated = NO;
+    [swizzle swizzleInstanceMethod:@selector(connection:didReceiveResponse:) withReplacementImplementation:^(APRequest *_self, NSURLConnection *connection, NSURLResponse *response){
+        assert(connection = _self.connection);
+        
+        [_self setResponse: response];
+        
+#ifdef __VERBOSE
+        if ([response isKindOfClass:[NSHTTPURLResponse class]])
+            NSLog(@"%ld", (long)((NSHTTPURLResponse *)response).statusCode);
+#endif
+        
+        NSError * error = nil;
+        [_self checkResponse:response error:&error];
+        
+        if (error) {
+            _self.error = error;
+            [[NSNotificationCenter defaultCenter] postNotificationName:kAPRequestErrorNotification
+                                                                object:_self
+                                                              userInfo:@{ kAPRequestErrorNotificationErrorKey: error }];
+        }
+
+        if (connectionReceievedResponseCallback) {
+            connectionReceievedResponseCallback(connection,response);
+        }
+        
+    }];
+}
+
++ (void)checkConnectionResponse:(BOOL (^)(NSURLResponse *response, NSError **error))checkConnectionResponseCallback{
+    Swizzlean *swizzle = [[Swizzlean alloc] initWithClassToSwizzle:[APRequest class]];
+    swizzle.resetWhenDeallocated = NO;
+    [swizzle swizzleInstanceMethod:@selector(checkResponse:error:) withReplacementImplementation:^(APRequest *_self, NSURLResponse *response, NSError **error){
+        
+        BOOL useDefaultChecks = YES;
+        
+        if (checkConnectionResponseCallback) {
+            useDefaultChecks = checkConnectionResponseCallback(response, error);
+        }
+        
+        if (useDefaultChecks){
+            NSHTTPURLResponse * httpResponse = (NSHTTPURLResponse *)response;
+            
+            if ([httpResponse isKindOfClass:[NSHTTPURLResponse class]]) {
+                if (httpResponse.statusCode < 200 || httpResponse.statusCode >= 300) {
+                    *error = [NSError errorWithDomain:kAPRequestErrorDomain
+                                                 code:kAPRequestErrorServerError
+                                             userInfo:@{ kAPRequestErrorURLResponseKey: response, kAPRequestErrorStatusCodeKey: @(httpResponse.statusCode), NSLocalizedDescriptionKey: [[NSHTTPURLResponse localizedStringForStatusCode:httpResponse.statusCode] titleize] }];
+                }
+            }
+            else if (*error) {
+                if ([*error code] == NSURLErrorUserCancelledAuthentication) {
+                    *error = [NSError errorWithDomain:kAPRequestErrorDomain
+                                                 code:kAPRequestErrorServerError
+                                             userInfo:@{ kAPRequestErrorStatusCodeKey: [NSNumber numberWithInt:401], NSLocalizedDescriptionKey: [[NSHTTPURLResponse localizedStringForStatusCode:401] titleize], NSUnderlyingErrorKey: *error }];
+                }
+            }
+        }
+        
+    }];
+}
+
++ (void)connectionFinished:(BOOL (^)(NSURLResponse *response, NSMutableData *downloadedData, NSError *error))connectionDidFinishCallback{
     Swizzlean *swizzle = [[Swizzlean alloc] initWithClassToSwizzle:[APRequest class]];
     swizzle.resetWhenDeallocated = NO;
     [swizzle swizzleInstanceMethod:@selector(connectionDidFinishLoading:) withReplacementImplementation:^(APRequest *_self, NSURLConnection *connection) {
         assert(connection == _self.connection);
         
-        if (sdkShouldLog) {
-            NSLog(@"%@", [[NSString alloc] initWithData:_self.downloadedData encoding:NSUTF8StringEncoding]);
-        }
+#ifdef __VERBOSE
+        NSLog(@"%@", [[NSString alloc] initWithData:_self.downloadedData encoding:NSUTF8StringEncoding]);
+#endif
         
         BOOL shouldContinue = true;
-        if (hook){
-           shouldContinue = hook(_self.downloadedData, _self.error);
+        if (connectionDidFinishCallback){
+            shouldContinue = connectionDidFinishCallback(_self.response, _self.downloadedData, _self.error);
         }
         
         if (shouldContinue) {
